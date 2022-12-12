a1c880ede10a56b08009d0ab0c824726e4b708c8
Marcelo Navarro <marcelo.nd.mx@gmail.com>
2022-12-12 17:47:44 +0100

    Rarefactoring using phyloseq started.

--- nose_diversity.R
+++ nose_diversity.R
@@ -4,78 +4,15 @@ library(ggplot2)
 
 source("C:/Users/Marcelo/Documents/Github/microbiome-help/diversity_data_helper_functions.R")
 
+setwd("C:/Users/Marcelo/Desktop/HMP_nose_data_analysis/qiime_analyses/qiime_analyses_asv")
+
 # ASVs
-# mac
-otu_table_path <- "/Users/marcelonavarrodiaz/Downloads/qiime_analises/qiime_analises_asv/3_resultados/7_table.from_biom_w_taxonomy_strain.txt"
-# windows
-otu_table_path <- "C:/Users/Marcelo/Desktop/HMP_nose_data_analysis/qiime_analises/qiime_analises_asv/3_resultados/7_table.from_biom_w_taxonomy_strain.txt"
+otu_table_path <- "./3_resultados/7_table.from_biom_w_taxonomy_strain.txt"
 
-# OTUs
-otu_table_path <- "/Users/marcelonavarrodiaz/Downloads/qiime_analises/qiime_analises_otu/3_resultados/8_table.from_biom_w_taxonomy_strain_level.txt"
+biom_path <- "./3_resultados/6_table_w_tax_strain.biom"
 
-# Helper functions
-greengenes_parser_strain_level <- function(string){
-  result_string <- ""
-  pieces <- strsplit(string, split  = ";")[[1]]
-  len_pieces <- length(pieces)
-  
-  pieces_counter <- len_pieces
-  # while result string is empty.
-  while(nchar(result_string) == 0){
-    # if we arrived to the end of the categories and not reached taxonomy return the original string, e.g. "Undetermined" (to make sure loop stops).
-    if(pieces_counter < 2){
-      result_string <- string
-    }else{
-      # lets analyze the last two pieces of the string
-      last_piece <- strsplit(pieces[pieces_counter], split  = "__")[[1]]
-      ap_piece <- strsplit(pieces[pieces_counter - 1], split  = "__")[[1]]
-      if (length(last_piece) == 2 && (last_piece[1] == " n" )) {
-        genus_piece <- strsplit(pieces[pieces_counter - 2], split  = "__")[[1]]
-        result_string <- paste(genus_piece[2], ap_piece[2], last_piece[2])
-      }
-      # if the last piece has length two (means is has a name on it), "s" means species. Ideal taxonomy resolution case.
-      else if(length(last_piece) == 2 && (last_piece[1] == " s" || last_piece[1] == "s")){
-        result_string <- paste(ap_piece[2], last_piece[2])
-      }else if(length(last_piece) == 2 && last_piece[1] != " s"){ #if last piece has a name but it is not species, add sp
-        result_string <- paste(last_piece[2], "sp")
-      }else if(length(ap_piece) == 2){ #if antepenultimate piece has a name, add sp
-        result_string = paste(ap_piece[2], "sp")
-      }
-    }
-    # we go to the next taxonomy level if we did not find a resolved taxonomy in these levels.
-    pieces_counter <- pieces_counter - 1
-  }
-  return(result_string)
-}
-
-read_qiime_otu_table2 <- function(table_path){
-  if (!"readr" %in% installed.packages()) install.packages("readr")
-  if (!"collections" %in% installed.packages()) install.packages("collections")
-  if (!"dplyr" %in% installed.packages()) install.packages("dplyr")
-  if (!"tibble" %in% installed.packages()) install.packages("tibble")
-  
-  # Read otu_table "as is"
-  user_table <- readr::read_delim(table_path, skip = 1 ,delim = "\t")
-  
-  # Getting a vector of parsed taxonomy
-  
-  tax_col <- apply(user_table["taxonomy"], 1, greengenes_parser_strain_level)
-
-  # Assigning taxonomy to column taxonomy
-  user_table["taxonomy"] <- tax_col
-  
-  # Moving tax column to the first column
-  user_table <- cbind(user_table[, ncol(user_table)], user_table[1:nrow(user_table), 2:(ncol(user_table)-1)])
-  
-  # Collapse all the ASVs with the same taxonomy assignation
-  user_table <- user_table %>%
-    group_by(taxonomy) %>%
-    summarise_all(sum)
-  
-  # Setting row names and dropping rownames column
-  user_table <- tibble::column_to_rownames(user_table, var = "taxonomy")
-  return(user_table)
-}
+load_test <- load_biom(biom_path, tax_rank = "Strain")
+load_test2 <- load_biom(biom_path, tax_rank = "Family")
 
 # Read table
 otu_table <- read_qiime_otu_table2(otu_table_path)
